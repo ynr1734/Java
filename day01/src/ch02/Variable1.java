28e30c6510198410132baa8fe509ffcfaa029559
ynr1734 <ynr1734@naver.com>
2022-04-19 23:33:57 +0900

    day01

---
+++ day01/src/ch02/Variable1.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int a = 2000000000;
+		a = 20;
+		int maxSpeed = 200;
+//		int for = 100;
+		System.out.println(a);
+	}
+
+}
