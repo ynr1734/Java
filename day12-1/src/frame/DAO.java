cbc0480ff1ccef210ce77d295d9f4526f9f1bbfc
ynr1734 <ynr1734@naver.com>
2022-05-02 17:39:41 +0900

    day12-1

---
+++ day12-1/src/frame/DAO.java
@@ -0,0 +1,14 @@
+package frame;
+
+import java.util.List;
+
+public interface DAO<K,V> {
+	
+	
+	// CRUD
+	public void insert(V v) throws Exception;
+	public void delete(K k) throws Exception;
+	public void update(V v) throws Exception;
+	public Object select(K k) throws Exception;
+	public List<V> select() throws Exception;
+}
